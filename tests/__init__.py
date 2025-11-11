0a92c1690a4be6b18fa5bdb4bbbb0d2f6584caef
Shree Dutt Pandey <sduttmay4@gmail.com>
2025-11-11 13:54:24 +0530

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,16 +1,1 @@
-import factory
-from factory.fuzzy import FuzzyChoice, FuzzyDecimal
-from service.models import Product, Category
 
-class ProductFactory(factory.Factory):
-    """Creates fake Products"""
-
-    class Meta:
-        model = Product
-
-    id = factory.Sequence(lambda n: n)
-    name = factory.Faker("word")
-    description = factory.Faker("sentence")
-    price = FuzzyDecimal(10.0, 100.0, precision=2)
-    available = FuzzyChoice(choices=[True, False])
-    category = FuzzyChoice(choices=[Category.CLOTHING, Category.FOOD, Category.ELECTRONICS, Category.HOUSEWARES, Category.TOYS])
